459334e31c4b8339948c06fa1580d72b75907081
Stu <stuartbrockman@gmail.com>
2024-12-05 14:06:40 +0000

    Copy .dat files as these have to be with the .exe now.

--- project/DOS/build.bat
+++ project/DOS/build.bat
@@ -6,4 +6,5 @@ set EDPATH=c:\watcom\eddat
 set WIPFC=c:\watcom\wipfc
 set LIBDOS=c:\watcom\lib286\dos;c:\watcom\lib286
 wmake.exe -h -e
+if "%ERRORLEVEL%"=="0" copy ..\..\*.dat
 if not "%ERRORLEVEL%"=="0" cmd /c exit 1
